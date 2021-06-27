52c1f436a0e1dc3fb8c51f9688499142194339d3
rishikagou <86407877+rishikagou@users.noreply.github.com>
2021-06-27 09:43:03 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  
+  return(
+  <div className="note">
+    <h1> Javascript and react.js </h1>
+    <p>A basic web dev Reacrt JsBootcamp</p>
+  </div>
+  );
+}
+ 
+export default Info;
